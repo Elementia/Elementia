539d48fdff55580728d5ca8dcff6478a3d8b0696
Michael Trittin <render.still@gmail.com>
2015-01-10 00:42:41 -0500

    Beta blocks/entities

---
+++ src/main/java/mod/elementia/blocks/ElementiaBlock.java
@@ -0,0 +1,12 @@
+package mod.elementia.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public abstract class ElementiaBlock extends Block {
+
+	protected ElementiaBlock(Material material) {
+		super(material);
+	}
+
+}
