ab562fe9a98359cb0d54f09547ecbc903ccab6cd
AliquotMesozoic <michaeltrittin@gmail.com>
2015-01-11 01:26:46 -0500

    Adding the src

---
+++ src/main/java/com/elementia/mod/Elementia.java
@@ -0,0 +1,13 @@
+package com.elementia.mod;
+
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = Elementia.MOD_ID, name = Elementia.MOD_NAME, version = Elementia.MOD_VERSION)
+public class Elementia {
+
+    public static final String MOD_ID = "ELementiaMod";
+    public static final String MOD_NAME = "Elementia";
+    public static final String MOD_VERSION = "1.7.10-1.0";
+
+}
