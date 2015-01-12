4902cbd0efd0f7a8fbef061a1d5c05fd9d13bcc7
AliquotMesozoic <michaeltrittin@gmail.com>
2015-01-12 16:36:28 -0500

    Adding jump 2x jump boots

---
+++ src/main/java/com/elementia/elementiajmt/items/armor/DoubleJumpBoots.java
@@ -0,0 +1,94 @@
+package mod.elementia.items.armor;
+
+import java.util.List;
+import static mod.elementia.util.NBTUtils.*;
+
+import mod.elementia.misc.CreativeTabElementia;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+
+public class DoubleJumpBoots extends ItemArmor {
+	/** The amount of jumps you have. Probably won't change. Maybe. */
+	private static byte jumpAmount = 1;
+
+	public DoubleJumpBoots(int armorSlot) {
+		super(ItemArmor.ArmorMaterial.DIAMOND, 0, 3);// locks it to the feet
+		setMaxDamage(1000);// ... I don't really know
+		this.setUnlocalizedName("doubleJumpBoots");
+		this.setCreativeTab(CreativeTabElementia.tabInstance);
+		// TODO textures
+	}
+
+	/**
+	 * 
+	 * @param world
+	 * @param player
+	 *            Who this is equipped to.
+	 * @param itemStack
+	 *            The stack of this type of item.
+	 */
+	@Override
+	public void onArmorTick(World world, EntityPlayer player,
+			ItemStack itemStack) {
+
+		// resets the jumps if the player is on the ground.
+		if (player.onGround) {
+			setJumps(itemStack, jumpAmount);
+		}
+
+		// checks if the player is off the ground, can jump, and if the velocity
+		// is around half of a normal jump, it allows you to double jump.
+		if (!player.onGround
+				&& Minecraft.getMinecraft().gameSettings.keyBindJump
+						.getIsKeyPressed() && player.motionY < .22
+				&& getJumps(itemStack) > 0) {
+			player.jump();
+			player.isAirBorne = false;
+			setJumps(itemStack, (byte) (getJumps(itemStack) - 1));
+
+			player.fallDistance = 0.0f;// TODO make this work
+		}
+		super.onArmorTick(world, player, itemStack);
+
+	}
+
+	/**
+	 * @param itemStack
+	 * @return the "jumps" byte value.
+	 */
+	private byte getJumps(ItemStack itemStack) {
+		NBTTagCompound tagCompound = getNBT(itemStack);
+		if (!tagCompound.hasKey("jumps")) {
+			tagCompound.setByte("jumps", jumpAmount);
+		}
+		return tagCompound.getByte("jumps");
+	}
+
+	/**
+	 * 
+	 * @param itemStack
+	 * @param jumps
+	 *            the byte in which the itemstack's "jumps" byte is stored.
+	 */
+	private void setJumps(ItemStack itemStack, byte jumps) {
+		getNBT(itemStack).setByte("jumps", jumps);
+	}
+
+	@Override
+	public void addInformation(ItemStack itemStack, EntityPlayer player,
+			List descList, boolean par4) {
+		descList.add("Allows the second use of a spacebar. Neat!");
+
+		super.addInformation(itemStack, player, descList, par4);
+	}
+
+	@Override
+	public boolean getIsRepairable(ItemStack p_82789_1_, ItemStack p_82789_2_) {
+		return false;
+	}
+
+}
---
+++ src/main/java/com/elementia/elementiajmt/util/NBTUtils.java
@@ -0,0 +1,29 @@
+package mod.elementia.util;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+
+/**
+ * Utils to help deal with NBT values. Probably will get more developed.
+ *
+ */
+public class NBTUtils {
+
+	/**
+	 * Returns an {@link NBTTagCompound} value from a stack of items passed into
+	 * the function, creating a new one if it does not exist. The TagCompound
+	 * can then be modified by passing arguments on the TagCompound itself, such
+	 * as {@link NBTTagCompound#getByte(String)} and
+	 * {@link NBTTagCompound#setLong(String, long)}.
+	 * 
+	 * @param itemStack
+	 * @return
+	 */
+	public static NBTTagCompound getNBT(ItemStack itemStack) {
+		if (itemStack.stackTagCompound == null) {
+			itemStack.stackTagCompound = new NBTTagCompound();
+		}
+		// TODO test itemStack.itemStack.setTagInfo(params);
+		return itemStack.stackTagCompound;
+	}
+}
